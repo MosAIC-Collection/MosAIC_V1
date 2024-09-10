3c67949f486de2346de3266dfd4daf544f2cec8f
Aidan Foo <aidanfoo96@gmail.com>
2024-09-10 09:12:10 +0100

    Commit changes new files and paths in scripts

--- 03_MosAIC_Phylogeny/plot_tree_metadata.R
+++ 03_MosAIC_Phylogeny/plot_tree_metadata.R
@@ -12,36 +12,36 @@ library(treeio)
 library(MetBrewer)
 
 ### Load the tree - phylogeny based on the 16s rRNA region of Mosquito Associated Bacteria
-tree_mos <- read.iqtree("03_MosAIC_Phylogeny/270423_concatenated_16s_fileappend_final_with_outgroup_aln_trimal.fa.contree")
-tree_mos_root <- root(tree_mos, outgroup = "16S_rRNA")
+tree_mos <- read.tree("MosAIC_V1/03_MosAIC_Phylogeny/270423_concatenated_16s_fileappend_final_with_outgroup_aln_trimal.fa.treefile")
+tree_mos_root <- tidytree::root(phy = tree_mos, outgroup = "16S_rRNA")
 
 ### Load the meta data of names and sample IDs corresponding to the nodes of the tree 
-dat1_mos <- read.csv("03_MosAIC_Phylogeny/MMO142ADJ_240223_tree_tibble_join_td_select.csv")
+dat1_mos <- read.csv("MosAIC_V1/03_MosAIC_Phylogeny/MMO142ADJ_240223_tree_tibble_join_td_select.csv")
 dat1_mos <- dat1_mos[1:142,]
 
 ### Load the metadata of the full table 
-dat2_mos <- read.csv("03_MosAIC_Phylogeny/MMO142ADJ_240223_071222_tree_tibble_drep_join_full_table.csv")
+dat2_mos <- read.csv("MosAIC_V1/03_MosAIC_Phylogeny/MMO142ADJ_240223_071222_tree_tibble_drep_join_full_table.csv")
 dat2_mos <- dat2_mos[1:142,]
 
 
 ### Make the first tree - fan layout and include bootstrap values
-p <- ggtree(tree_mos, layout="fan", size=0.2, open.angle=20) + 
-  geom_point2(aes(subset=!isTip & UFboot > 80, x = branch, col = UFboot), size = 0.7, na.rm = T) + 
-  scale_color_gradientn(colors = met.brewer("Pillement", n = 3))
-  #geom_hilight(data=nodedf, mapping=aes(node=node),
-               #extendto=8, alpha=0.5, fill="grey", color="grey50",
-               #size=0.01) 
-  #geom_cladelab(data=labdf, 
-                #mapping=aes(node=node, 
-                            #label=label,
-                            #offset.text=pos),
-                #hjust=0.5,
-                #angle="auto",
-                #barsize=NA,
-                #horizontal=FALSE, 
-                #fontsize=1.4,
-                #fontface="italic"
-  #)
+ggtree(tree_mos_root, layout="fan", size=0.2, open.angle=20) 
+#geom_point2(aes(subset=!isTip & UFboot > 80, x = branch, col = UFboot), size = 0.7, na.rm = T) + 
+#scale_color_gradientn(colors = met.brewer("Pillement", n = 3))
+#geom_hilight(data=nodedf, mapping=aes(node=node),
+#extendto=8, alpha=0.5, fill="grey", color="grey50",
+#size=0.01) 
+#geom_cladelab(data=labdf, 
+#mapping=aes(node=node, 
+#label=label,
+#offset.text=pos),
+#hjust=0.5,
+#angle="auto",
+#barsize=NA,
+#horizontal=FALSE, 
+#fontsize=1.4,
+#fontface="italic"
+#)
 
 
 p1 <- p + new_scale_color() 
@@ -52,13 +52,13 @@ p1 <- p1 %<+% dat1_mos + geom_point2(
   mapping=aes(col=class),
   position="identity") +
   scale_colour_manual(values=c("#FFC125","#87CEFA","#7B68EE","#808080",
-                             "#800080", "#9ACD32","#D15FEE","#FFC0CB",
-                             "#EE6A50","#8DEEEE", "#006400","#800000",
-                             "#B0171F","#191970"),
-                    guide=guide_legend(keywidth = 0.5, 
-                                       keyheight = 0.5, order=1,
-                                       override.aes=list(starshape=15)),
-                    na.translate=FALSE) +
+                               "#800080", "#9ACD32","#D15FEE","#FFC0CB",
+                               "#EE6A50","#8DEEEE", "#006400","#800000",
+                               "#B0171F","#191970"),
+                      guide=guide_legend(keywidth = 0.5, 
+                                         keyheight = 0.5, order=1,
+                                         override.aes=list(starshape=15)),
+                      na.translate=FALSE) +
   scale_starshape_manual(values=c(15, 1),
                          guide=guide_legend(keywidth = 0.5, 
                                             keyheight = 0.5, order=2),
@@ -117,17 +117,17 @@ p7 <- gheatmap(p6, lab_field, width = 0.05, offset = 0.1, colnames = FALSE) +
 ### Fifth Layer to get number of isolates within each cluster
 p8 <- p7  + new_scale_fill() +
   #geom_fruit(data=dat2_mos, geom=geom_tile,
-             #mapping=aes(y=label, x=domain, fill=class),
-             #color = "grey50", size = 0.5) +
+  #mapping=aes(y=label, x=domain, fill=class),
+  #color = "grey50", size = 0.5) +
   #scale_alpha_continuous(range=c(0, 1),
-                         #guide=guide_legend(keywidth = 0.3, 
-                                            #keyheight = 0.3, order=5)) +
+  #guide=guide_legend(keywidth = 0.3, 
+  #keyheight = 0.3, order=5)) +
   geom_fruit(
     data=dat2_mos, 
     geom=geom_bar, 
     mapping=aes(
       y=label, x=cluster_members
-      ),
+    ),
     pwidth=0.5,
     offset = 0.25, 
     orientation="y", 
@@ -141,9 +141,9 @@ p8 <- p7  + new_scale_fill() +
     grid.params = list()
   ) +
   #scale_fill_manual(values=c("#0000FF","#FFA500","#FF0000",
-                             #"#800000", "#006400","#800080","#696969"),
-                    #guide=guide_legend(keywidth = 0.3, 
-                                       #keyheight = 0.3, order=4))
+  #"#800000", "#006400","#800080","#696969"),
+  #guide=guide_legend(keywidth = 0.3, 
+  #keyheight = 0.3, order=4))
   geom_treescale(fontsize=2, linesize=0.1, x=1, y=0.05) +
   theme(legend.position=c(1.05, 0.5),
         legend.background=element_rect(fill=NA),
@@ -151,9 +151,9 @@ p8 <- p7  + new_scale_fill() +
         legend.text=element_text(size=10),
         legend.spacing.y = unit(0.2, "cm"),
   ) #+ 
-  #geom_fruit(data=dat2_mos, geom=geom_tile, 
-             #mapping = aes(y = label, x = source, fill = source), 
-             #offset = 0.0001)
+#geom_fruit(data=dat2_mos, geom=geom_tile, 
+#mapping = aes(y = label, x = source, fill = source), 
+#offset = 0.0001)
 
 
 
@@ -170,9 +170,11 @@ ggsave(filename = "plots/PlotsFinal/Supplementary_Figure_11_Extended_Metadata_Mo
 #### Create another tree showing class classifications, bootstrap, family, and cluster number 
 #### More representative of the dereplicated genomes
 
-pv2 <- ggtree(tree_mos_root, layout="circular", size=0.2, open.angle=20) + 
-  geom_point2(aes(subset=!isTip & UFboot > 80, x = branch, col = UFboot), size = 0.7, na.rm = T) + 
-  scale_color_gradientn(colors = met.brewer("Pillement", n = 3))
+pv2 <- ggtree(tree_mos_root, layout="fan", size=0.2, open.angle=20) 
+#geom_point2(aes(subset=!isTip & UFboot > 80, x = branch, col = UFboot), size = 0.7, na.rm = T) + 
+#scale_color_gradientn(colors = met.brewer("Pillement", n = 3))
+#geom_point2(aes(subset=!isTip & UFboot > 80, x = branch, col = UFboot), size = 0.7, na.rm = T) + 
+#scale_color_gradientn(colors = met.brewer("Pillement", n = 3))
 
 pv2 <- pv2 + new_scale_color() 
 
@@ -201,13 +203,13 @@ pv2_2 <- gheatmap(pv2_1, family_data, width = 0.05, offset = 0.0, colnames_posit
   scale_fill_manual(values = c("#005C66", "#BD5129", "#9FBAE7", "#1000D2", "#0070C8", "#52CEFF", 
                                "#009464", "#E1B698", "#06BF34", "#9D49AB", "#94E4FF", "#E3F8FF", 
                                "#A2581B", "#00B9C8", "#9AC7D9", "#50E4BC", "#6E2A4D", "#D582D2", 
-                               "#F0AC7A", "#F7D7EF", "#97ADFF", "#7500FF", "#FFF4B1", "#52E685", 
+                               "#F0AC7A", "#F7D7EF", "#176E93", "#7500FF", "#FFF4B1", "#52E685", 
                                "#FF9146", "#E0FFF1", "#A3A3FF", "#FFAAB7", "#FFE7CC")) + 
   guides(fill = guide_legend(ncol = 1, title = "Classification", label.position = "bottom")) 
-  
+
 ggsave(pv2_2, filename = "plots/PlotsFinal/Figure2_ALT_LegendKey.pdf", width = 14, heigh = 35)
 
-  pv2_3 <- pv2_2  + new_scale_fill() +
+pv2_3 <- pv2_2  + new_scale_fill() +
   #geom_fruit(data=dat2_mos, geom=geom_tile,
   #mapping=aes(y=label, x=domain, fill=class),
   #color = "grey50", size = 0.5) +
@@ -232,16 +234,16 @@ ggsave(pv2_2, filename = "plots/PlotsFinal/Figure2_ALT_LegendKey.pdf", width = 1
     ),
     grid.params = list(size = 0.5)
   ) +
-    scale_fill_manual(values = c("#005C66", "#BD5129", "#9FBAE7", "#1000D2", "#0070C8", "#52CEFF", 
-                                 "#009464", "#E1B698", "#06BF34", "#9D49AB", "#94E4FF", "#E3F8FF", 
-                                 "#A2581B", "#00B9C8", "#9AC7D9", "#50E4BC", "#6E2A4D", "#D582D2", 
-                                 "#F0AC7A", "#F7D7EF", "#97ADFF", "#7500FF", "#FFF4B1", "#52E685", 
-                                 "#FF9146", "#E0FFF1", "#A3A3FF", "#FFAAB7", "#FFE7CC")) + 
+  scale_fill_manual(values = c("#005C66", "#BD5129", "#9FBAE7", "#1000D2", "#0070C8", "#52CEFF", 
+                               "#009464", "#E1B698", "#06BF34", "#9D49AB", "#94E4FF", "#E3F8FF", 
+                               "#A2581B", "#00B9C8", "#9AC7D9", "#50E4BC", "#6E2A4D", "#D582D2", 
+                               "#F0AC7A", "#F7D7EF", "#97ADFF", "#7500FF", "#FFF4B1", "#52E685", 
+                               "#FF9146", "#E0FFF1", "#A3A3FF", "#FFAAB7", "#FFE7CC")) + 
   #scale_fill_manual(values=c("#0000FF","#FFA500","#FF0000",
   #"#800000", "#006400","#800080","#696969"),
   #guide=guide_legend(keywidth = 0.3, 
   #keyheight = 0.3, order=4))
-  geom_treescale(fontsize=2, linesize=0.1, x=0.5, y=-2) +
+  geom_treescale(fontsize=5, linesize=0.1, x=0.5, y=-2) +
   theme(legend.position=c(1.3, 0.5),
         legend.background=element_rect(fill=NA),
         legend.title=element_blank(),
@@ -252,7 +254,7 @@ ggsave(pv2_2, filename = "plots/PlotsFinal/Figure2_ALT_LegendKey.pdf", width = 1
 #mapping = aes(y = label, x = source, fill = source), 
 #offset = 0.0001)
 
-ggsave(filename = "plots/PlotsFinal/Figure_2_MosAIC_dRep_Cluster_Phylogeny_Circular_Bootstraps.pdf", plot = pv2_3, height = 10, width = 22)
+ggsave(filename = "plots/PlotsFinal/Figure_2_MosAIC_dRep_Cluster_Phylogeny.pdf", plot = pv2_3, height = 10, width = 22)
 
 
 
